b45e6588082f14e9a5b8c54bf00b0f2f35e5acf5
KRAT00S1 <ra54029168884@acaonsfatima.org.br>
2025-10-20 08:39:14 -0300

    update

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import cors from 'cors'
 import Rotas from './rotas.js'
+import multer from 'multer'
 
 const api = express()
+api.use(multer(de))
 api.use(express.json())
 api.use(cors())
 
