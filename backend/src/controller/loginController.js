a2417bbd0706f4e82c0ca97d472010d8049fde16
KRAT00S1 <ra54029168884@acaonsfatima.org.br>
2025-11-07 11:58:58 -0300

    pp

--- backend/src/controller/loginController.js
+++ backend/src/controller/loginController.js
@@ -1,13 +1,34 @@
 import { Router } from "express";
-import { Listar, Login } from "../repository/loginRepository.js";
+import { cadastrar, Listar, verificar } from "../repository/loginRepository.js";
+import { generateToken } from "../utils/jwt.js";
 const endpoint = Router()
 
 
-endpoint.post('/login', async (req,resp) => {
-    let novo = req.body
-    let id = await Login(novo)
-    resp.send({NovoId:id});
+endpoint.post('/cadastrar', async (req,resp)=>{
+    let body = req.body
+    let reg = await cadastrar(body)
+
+    resp.send({novoId:reg})
 })
+
+
+
+endpoint.post('/login', async (req,resp)=>{
+    let body = req.body
+    let reg = await verificar(body)
+
+    if(!reg){
+        resp.status(401).send({erro:'Login inválido'})
+    }
+
+    resp.send({
+        usuario: body.email,
+        token: generateToken(body)
+    })
+
+})
+
+
 
 endpoint.get('/listar', async (req,resp)=>{
     let reg = await Listar()
--- backend/src/repository/loginRepository.js
+++ backend/src/repository/loginRepository.js
@@ -1,16 +1,31 @@
 import { connection } from "./connection.js"
 
-export async function Login(novo) {
-    const comando = `
-        insert into Login(usuario, senha)
-        values
-        (?,MD5(?))
-    `
 
-    const [info] = await connection.query(comando,[novo.usuario, novo.senha])
+export async function cadastrar(novo) {
+    const [info] = await connection.query(
+        `Insert into lOGIN (nome,email,senha)
+        values(?,?,MD%(?))`,
+        [novo.nome, novo.email, novo.senha]
+    )
+
     return info.insertId
+
 }
 
+
+
+export async function verificar(pessoa) {
+    const comando = `
+selectnome,
+       email
+  from login
+ where email = ?
+   and senha = MD5(?)
+`
+
+    const [info] = await connection.query(comando, [pessoa.email, pessoa.senha])
+    return info[0]
+}
 
 export async function Listar() {
     const comando = `
